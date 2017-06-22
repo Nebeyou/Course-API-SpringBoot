a6972237676d4d348ca511ea31c0503e28684694
Nebeyou <Nebeyou2007@gmail.com>
2017-06-21 23:42:41 -0500

    Only added comment on repository

--- Course-api-data/src/main/java/org/neba/TopicRespository.java
+++ Course-api-data/src/main/java/org/neba/TopicRespository.java
@@ -3,5 +3,5 @@ package org.neba;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRespository extends CrudRepository<Topic, String> {
-	
+	//no need to add any method unless you want specifc implementation 
 }
